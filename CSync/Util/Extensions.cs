a7a868e1ffe7566069687f19772292c190497821
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-27 08:56:48 +0000

    fix the bind overloads and remove the shit one

--- CSync/Util/Extensions.cs
+++ CSync/Util/Extensions.cs
@@ -1,7 +1,5 @@
 using BepInEx.Configuration;
 using CSync.Lib;
-using System.Runtime.Serialization;
-using Unity.Netcode;
 
 namespace CSync.Util;
 
@@ -12,33 +10,47 @@ public static class Extensions {
     /// <summary>
     /// Binds an entry to this file and returns the converted synced entry.
     /// </summary>
-    /// <param name="cfg">The currently selected config file.</param>
+    /// <param name="configFile">The currently selected config file.</param>
     /// <param name="section">The category that this entry should show under.</param>
     /// <param name="key">The name/identifier of this entry.</param>
     /// <param name="defaultVal">The value assigned to this entry if not changed.</param>
-    /// <param name="desc">The description indicating what this entry does.</param>
-    public static SyncedEntry<V> BindSyncedEntry<V>(this ConfigFile cfg, 
-        string section, string key, V defaultVal, string desc
+    /// <param name="description">The description indicating what this entry does.</param>
+    public static SyncedEntry<V> BindSyncedEntry<V>(
+        this ConfigFile configFile,
+        string section,
+        string key,
+        V defaultVal,
+        string description
     ) {
-        return cfg.Bind(section, key, defaultVal, desc).ToSyncedEntry();
+        return configFile.BindSyncedEntry(new ConfigDefinition(section, key), defaultVal, new ConfigDescription(description));
     }
 
-    public static SyncedEntry<V> BindSyncedEntry<V>(this ConfigFile cfg, 
-        string section, string key, V defaultVal, ConfigDescription desc
+    public static SyncedEntry<V> BindSyncedEntry<V>(
+        this ConfigFile configFile,
+        string section,
+        string key,
+        V defaultValue,
+        ConfigDescription? desc = null
     ) {
-        return cfg.BindSyncedEntry(section, key, defaultVal, desc.Description);
+        return configFile.BindSyncedEntry(new ConfigDefinition(section, key), defaultValue, desc);
     }
 
-    public static SyncedEntry<T> BindSyncedEntry<T>(this ConfigFile cfg,
-        ConfigDefinition definition, T defaultValue, ConfigDescription desc = null
+    public static SyncedEntry<V> BindSyncedEntry<V>(
+        this ConfigFile configFile,
+        ConfigDefinition definition,
+        V defaultValue,
+        string description
     ) {
-        return cfg.BindSyncedEntry(definition.Section, definition.Key, defaultValue, desc.Description);
+        return configFile.BindSyncedEntry(definition, defaultValue, new ConfigDescription(description));
     }
 
-    public static SyncedEntry<T> BindSyncedEntry<T>(this ConfigFile cfg,
-        ConfigDefinition definition, T defaultValue, string desc
+    public static SyncedEntry<V> BindSyncedEntry<V>(
+        this ConfigFile configFile,
+        ConfigDefinition definition,
+        V defaultValue,
+        ConfigDescription? description = null
     ) {
-        return cfg.BindSyncedEntry(definition.Section, definition.Key, defaultValue, desc);
+        return configFile.Bind(definition, defaultValue, description).ToSyncedEntry();
     }
 
     /// <summary>
@@ -46,32 +58,5 @@ public static class Extensions {
     /// </summary>
     public static SyncedEntry<V> ToSyncedEntry<V>(this ConfigEntry<V> entry) {
         return new SyncedEntry<V>(entry);
-    }
-
-    /// <summary>
-    /// Helper method to grab a value from SerializationInfo and cast it to the specified type.
-    /// </summary>
-    public static T GetObject<T>(this SerializationInfo info, string key) {
-        return (T) info.GetValue(key, typeof(T));
-    }
-
-    internal static ConfigEntry<V> Reconstruct<V>(this ConfigFile cfg, SerializationInfo info) {
-        ConfigDefinition definition = new(info.GetString("Section"), info.GetString("Key"));
-        ConfigDescription description = new(info.GetString("Description"));
-
-        return cfg.Bind(definition, info.GetObject<V>("DefaultValue"), description);
-    }
-
-    internal static void SendMessage(this FastBufferWriter stream, string guid, string label, ulong clientId = 0uL) {
-        bool fragment = stream.Capacity > 1300;
-        NetworkDelivery delivery = fragment ? NetworkDelivery.ReliableFragmentedSequenced : NetworkDelivery.Reliable;
-
-        if (fragment) Plugin.Logger.LogDebug(
-            $"{guid} - Size of stream ({stream.Capacity}) was past the max buffer size.\n" +
-            "Config instance will be sent in fragments to avoid overflowing the buffer."
-        );
-
-        var msgManager = NetworkManager.Singleton.CustomMessagingManager;
-        msgManager.SendNamedMessage($"{guid}_{label}", clientId, stream, delivery);
     }
 }
