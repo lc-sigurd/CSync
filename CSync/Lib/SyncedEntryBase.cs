08b161210c01f95624283a31bfeb56f268c3e8a1
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-28 07:00:44 +0000

    add synced entry base class

---
+++ CSync/Lib/SyncedEntryBase.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using BepInEx.Configuration;
+using CSync.Util;
+
+namespace CSync.Lib;
+
+public abstract class SyncedEntryBase
+{
+    public abstract ConfigEntryBase BoxedEntry { get; protected init; }
+
+    public abstract object? BoxedValueOverride { get; set; }
+    protected internal bool ValueOverridden = false;
+
+    internal SyncedEntryBase(ConfigEntryBase configEntry)
+    {
+        BoxedEntry = configEntry;
+        BoxedValueOverride = configEntry.DefaultValue;
+    }
+
+    public void SetSerializedValueOverride(string value)
+    {
+        BoxedValueOverride = TomlTypeConverter.ConvertToValue(value, BoxedEntry.SettingType);
+    }
+
+    internal SyncedEntryDelta ToDelta() => new SyncedEntryDelta(
+        configFileRelativePath: Path.GetFileName(BoxedEntry.ConfigFile.ConfigFilePath),
+        definition: BoxedEntry.Definition.ToSynced(),
+        serializedValue: BoxedEntry.GetSerializedValue()
+    );
+}
