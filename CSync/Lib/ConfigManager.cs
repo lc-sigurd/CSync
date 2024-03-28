fc822cb89ada6e3372be2991821e4d73a8da60f3
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-28 07:02:23 +0000

    make a prefab, do nice stuff with the config file cache

--- CSync/Lib/ConfigManager.cs
+++ CSync/Lib/ConfigManager.cs
@@ -1,8 +1,13 @@
+using System;
 using BepInEx.Configuration;
 using BepInEx;
 using System.Collections.Generic;
 using System.IO;
-using HarmonyLib;
+using System.Security.Cryptography;
+using System.Text;
+using CSync.Util;
+using Unity.Netcode;
+using UnityEngine;
 
 namespace CSync.Lib;
 
@@ -11,46 +16,69 @@ namespace CSync.Lib;
 /// Handles config registration, instance syncing and caching of BepInEx files.<br></br>
 /// </summary>
 public class ConfigManager {
-    internal static Dictionary<string, ConfigFile> FileCache = [];
-    internal static Dictionary<string, ISynchronizable> Instances = [];
+    internal static readonly Dictionary<string, ConfigFile> FileCache = [];
+    internal static readonly Dictionary<string, ISyncedConfig> Instances = [];
 
-    internal static ConfigFile GetConfigFile(string fileName) {
-        bool exists = FileCache.TryGetValue(fileName, out ConfigFile cfg);
-        if (!exists) {
-            string absPath = Path.Combine(Paths.ConfigPath, fileName);
+    private static readonly Lazy<GameObject> LazyPrefab;
+    internal static GameObject Prefab => LazyPrefab.Value;
 
-            cfg = new(absPath, false);
-            FileCache.Add(fileName, cfg);
-        }
+    static ConfigManager()
+    {
+        LazyPrefab = new Lazy<GameObject>(() =>
+        {
+            var container = new GameObject("CSyncPrefabContainer")
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+            container.SetActive(false);
+            UnityEngine.Object.DontDestroyOnLoad(container);
 
-        return cfg;
+            var prefab = new GameObject("ConfigSyncHolder");
+            prefab.transform.SetParent(container.transform);
+            var networkObject = prefab.AddComponent<NetworkObject>();
+            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes($"{MyPluginInfo.PLUGIN_GUID}:ConfigSyncHolder"));
+            networkObject.GlobalObjectIdHash = BitConverter.ToUInt32(hash);
+
+            return prefab;
+        });
+    }
+
+    internal static void AddToFileCache(ConfigFile configFile)
+    {
+        FileCache.TryAdd(configFile.GetConfigFileRelativePath(), configFile);
+    }
+
+    internal static ConfigFile GetConfigFile(string relativePath)
+    {
+        if (FileCache.TryGetValue(relativePath, out ConfigFile configFile))
+            return configFile;
+
+        string absolutePath = Path.GetFullPath(Path.Combine(Paths.BepInExRootPath, relativePath));
+        configFile = new(absolutePath, false);
+        FileCache.Add(relativePath, configFile);
+        return configFile;
     }
 
     /// <summary>
     /// Register a config with CSync, making it responsible for synchronization.<br></br>
     /// After calling this method, all clients will receive the host's config upon joining.
     /// </summary>
-    public static void Register<T>(T config) where T : SyncedConfig<T>, ISynchronizable {
+    public static void Register<T>(T config) where T : SyncedConfig<T> {
+        if (config is null)
+        {
+            throw new ArgumentNullException(nameof(config), "Config instance is null, cannot register.");
+        }
+
         string guid = config.GUID;
 
-        if (config == null) {
-            Plugin.Logger.LogError($"An error occurred registering config: {guid}\nConfig instance cannot be null!");
+        try {
+            Instances.Add(guid, config);
+        }
+        catch (ArgumentException exc) {
+            throw new InvalidOperationException($"Attempted to register config `{guid}`, but it has already been registered.", exc);
         }
 
-        if (Instances.ContainsKey(guid)) {
-            Plugin.Logger.LogWarning($"Attempted to register config `{guid}` after it has already been registered!");
-            return;
-        }
-
-        //config.InitInstance(config);
-        Instances.Add(guid, config);
+        var syncBehaviour = Prefab.AddComponent<ConfigSyncBehaviour>();
+        syncBehaviour.Config = config;
     }
-
-    internal static void SyncInstances() => Instances.Values.Do(i => i.SetupSync());
-    internal static void RevertSyncedInstances() => Instances.Values.Do(i => i.RevertSync());
-}
-
-public interface ISynchronizable {
-    void SetupSync();
-    void RevertSync();
 }
