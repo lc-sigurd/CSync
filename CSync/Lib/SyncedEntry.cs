29726b27b8c563893df64070872cbdd284b13687
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-28 07:00:58 +0000

    inherit from the baseclass + do some wacky stuff

--- CSync/Lib/SyncedEntry.cs
+++ CSync/Lib/SyncedEntry.cs
@@ -1,7 +1,5 @@
 using System;
-using System.IO;
 using BepInEx.Configuration;
-using CSync.Util;
 
 namespace CSync.Lib;
 
@@ -9,45 +7,47 @@ namespace CSync.Lib;
 /// Wrapper class around a BepInEx <see cref="ConfigEntry{T}"/>.<br>
 /// Can serialize and deserialize itself to avoid runtime errors when syncing configs.</br>
 /// </summary>
-public class SyncedEntry<V>
+public sealed class SyncedEntry<T> : SyncedEntryBase
 {
-    public ConfigEntry<V> Entry { get; }
+    public ConfigEntry<T> Entry { get; private set; }
 
-    public V LocalValue
+    public override ConfigEntryBase BoxedEntry
+    {
+        get => Entry;
+        protected init => Entry = (ConfigEntry<T>) value;
+    }
+
+    public T LocalValue
     {
         get => Entry.Value;
         set => Entry.Value = value!;
     }
 
-    protected V _valueOverride;
-    protected bool _valueOverridden;
+    private T _typedValueOverride;
 
-    public V Value {
+    public override object? BoxedValueOverride
+    {
+        get => _typedValueOverride;
+        set => _typedValueOverride = (T) value!;
+    }
+
+    public T Value {
         get {
-            if (_valueOverridden) return _valueOverride;
+            if (ValueOverridden) return _typedValueOverride!;
             return LocalValue;
         }
     }
 
-    public static implicit operator V(SyncedEntry<V> e) => e.Value;
+    public static implicit operator T(SyncedEntry<T> e) => e.Value;
 
     public event EventHandler SettingChanged {
         add => Entry.SettingChanged += value;
         remove => Entry.SettingChanged -= value;
     }
 
-    public SyncedEntry(ConfigEntry<V> entry)
-    {
-        Entry = entry;
-    }
+    public SyncedEntry(ConfigEntry<T> entry) : base(entry) { }
 
     public override string ToString() {
         return $"Key: {Entry.Definition.Key}\nLocal Value: {LocalValue}\nCurrent Value: {Value}";
     }
-
-    internal SyncedEntryDelta ToDelta() => new SyncedEntryDelta(
-        configFileName: Path.GetFileName(Entry.ConfigFile.ConfigFilePath),
-        definition: Entry.Definition.ToSynced(),
-        serializedValue: TomlTypeConverter.ConvertToString(LocalValue, typeof(V))
-    );
 }
