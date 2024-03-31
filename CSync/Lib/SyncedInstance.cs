0465a5c4d48aa0cdebdd0d71c545dbeabd64d078
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-30 18:54:40 -0600

    add `.Instance` back to `SyncedInstance`

--- CSync/Lib/SyncedInstance.cs
+++ CSync/Lib/SyncedInstance.cs
@@ -2,4 +2,7 @@ using CSync.Util;
 
 namespace CSync.Lib;
 
-public class SyncedInstance<T> : ByteSerializer<T>;
+public class SyncedInstance<T> : ByteSerializer<T>
+{
+    public T? Instance { get; internal set; }
+}
