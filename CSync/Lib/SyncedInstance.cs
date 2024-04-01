2df52f994fbca3e4a9369b3a61711f67643aeabe
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-04-01 16:49:28 +0100

    mark stuff as obsolete

--- CSync/Lib/SyncedInstance.cs
+++ CSync/Lib/SyncedInstance.cs
@@ -1,9 +1,12 @@
+using System;
 using CSync.Util;
 
 namespace CSync.Lib;
 
 public class SyncedInstance<T> : ByteSerializer<T> where T : class
 {
+    [Obsolete]
     public static T? Instance { get; internal set; }
+    [Obsolete("This will never actually hold the local configured values. Use SyncedEntry.LocalValue instead.")]
     public static T? Default { get; internal set; }
 }
