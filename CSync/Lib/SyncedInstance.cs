bbcdc4185a5fdb242aa732ef706021653b8ad7b9
Joe Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-04-01 04:09:28 -0600

    Add `.Default`

--- CSync/Lib/SyncedInstance.cs
+++ CSync/Lib/SyncedInstance.cs
@@ -5,4 +5,5 @@ namespace CSync.Lib;
 public class SyncedInstance<T> : ByteSerializer<T> where T : class
 {
     public static T? Instance { get; internal set; }
+    public static T? Default { get; internal set; }
 }
